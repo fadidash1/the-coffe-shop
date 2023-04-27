adccfa15179d0d51fcf8cfa1d0ed3f291c3c62b1
albitar22 <fadi.albitar@grupposcai.it>
2023-04-27 15:44:40 +0200

    user service on going

--- demo/demo/src/main/java/com/example/demo/persistence/controller/UserController.java
+++ demo/demo/src/main/java/com/example/demo/persistence/controller/UserController.java
@@ -4,6 +4,9 @@ package com.example.demo.persistence.controller;
 import com.example.demo.persistence.entity.User;
 import com.example.demo.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,23 +20,23 @@ public class UserController {
 
     @GetMapping("/")
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+        return UserRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable int id) {
-        return userRepository.findById(id);
+        return UserRepository.findById(id);
     }
 
     @PostMapping("/")
     public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+        return UserRepository.save(user);
     }
 
     @PutMapping("/{id}")
     public User updateUser(@PathVariable int id, @RequestBody User user) {
         user.setId(id);
-        return userRepository.save(user);
+        return UserRepository.save(user);
     }
 
     @DeleteMapping("/{id}")
